7c15cbc3e6bd0501fd96c1d8dd721c912962ab42
Aswin <aswinpalapetty@gmail.com>
2023-12-02 08:14:44 +0530

    server: vercel.json file added, changes made in cors.

--- server/src/index.js
+++ server/src/index.js
@@ -20,7 +20,13 @@ initScheduler();
 
 const app = express()
 
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://cet-indoor-management-server.vercel.app/"],
+        methods: ["POST","GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 app.use("/student", student);
 app.use("/staff", staff);
